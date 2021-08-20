ca75552b222247412a122a7c6a75ef0fe3a39614
Fotios Tsakiris <footios76@gmail.com>
2021-08-20 22:43:40 +0300

    added icons to sidedrawer menu

--- src/navigation/Menu.tsx
+++ src/navigation/Menu.tsx
@@ -1,5 +1,11 @@
+import { FaHome } from 'react-icons/fa'
+import { AiFillSetting } from 'react-icons/ai'
+import { GiPodiumWinner } from 'react-icons/gi'
+import { BiLogOut, BiLogIn } from 'react-icons/bi'
+import { IoIosCreate } from 'react-icons/io'
 import { NavLink } from "react-router-dom";
 import '../styles/menu.scss';
+
 
 export default function Menu({ menuOpen, setMenuOpen }: { menuOpen: boolean, setMenuOpen: Function }) {
 
@@ -10,25 +16,34 @@ export default function Menu({ menuOpen, setMenuOpen }: { menuOpen: boolean, set
     <div className={`menu ${menuOpen && "active"} `}>
       <ul>
         <li>
-          <NavLink className='link' onClick={menuHandler} to="/en-touto-nika">ΕΝ ΤΟΥΤΩ ΝΙΚΑ</NavLink>
+          <NavLink className='link' onClick={menuHandler} to="/en-touto-nika">
+            <FaHome className='menu-icon' /> <p> Αρχική </p>
+          </NavLink>
         </li>
         <li>
-          <NavLink className='link' onClick={menuHandler} to="/winners">Νικητές</NavLink>
+          <NavLink className='link' onClick={menuHandler} to="/winners">
+            <GiPodiumWinner className='menu-icon' /> <p> Νικητές </p>
+          </NavLink>
         </li>
         <li>
-          <NavLink className='link' onClick={menuHandler} to="/settings">Ρυθμίσεις</NavLink>
+          <NavLink className='link' onClick={menuHandler} to="/settings">
+            <AiFillSetting className='menu-icon' /> <p>Ρυθμίσεις</p>
+          </NavLink>
         </li>
         <li>
-          <NavLink className='link' onClick={menuHandler} to="/create">Δημιουργία</NavLink>
+          <NavLink className='link' onClick={menuHandler} to="/create">
+            <IoIosCreate className='menu-icon' /> <p>Δημιουργία</p>
+          </NavLink>
         </li>
         <li>
-          <NavLink className='link' onClick={menuHandler} to="/logout">Έξοδος</NavLink>
+          <NavLink className='link' onClick={menuHandler} to="/logout">
+            <BiLogOut className='menu-icon' /> <p>Έξοδος</p>
+          </NavLink>
         </li>
         <li>
-          <NavLink className='link' onClick={menuHandler} to="/login">Είσοδος</NavLink>
-        </li>
-        <li>
-          <NavLink className='link' onClick={menuHandler} to="/register">Εγγραφή</NavLink>
+          <NavLink className='link' onClick={menuHandler} to="/register">
+            <BiLogIn className='menu-icon' /> <p>Είσοδος</p>
+          </NavLink>
         </li>
       </ul>
     </div>
