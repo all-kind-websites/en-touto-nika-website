8191176b1b586f11faeed17bdc2c5099d01cd58c
Fotios Tsakiris <footios76@gmail.com>
2021-08-26 09:39:57 +0300

    navigate user back to home page if he tries to navigate to register while logged in

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,8 +1,20 @@
-import LoginCard from '../../components/UI/LoginCard'
+
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import TheotokosIcon from '../../components/UI/TheotokosIcon'
+import LoginCard from '../../components/UI/LoginCard'
 import '../../styles/register.scss';
+import asyncNames from '../../constants/asyncNames';
 
 export default function Register() {
+  const history = useHistory();
+
+  const userIsLoggedIn = !!localStorage.getItem(asyncNames.userData);
+  useEffect(() => {
+    if (userIsLoggedIn) history.replace('/')
+  }, [userIsLoggedIn, history]);
+
   return (
     <div className='register' >
       <TheotokosIcon />
