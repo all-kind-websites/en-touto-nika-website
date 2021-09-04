0797893bcc8478e9fe2b68308628cfcf414b7942
Fotios Tsakiris <footios76@gmail.com>
2021-09-04 15:31:20 +0300

    added a promt also in GameOverScreen

--- src/pages/extras/GameOverPage.tsx
+++ src/pages/extras/GameOverPage.tsx
@@ -1,3 +1,4 @@
+import { Prompt } from 'react-router-dom'
 
 import { useState } from "react";
 import { RootStateOrAny, useSelector } from "react-redux";
@@ -27,9 +28,8 @@ const GameOverPage = ({
   totalPoints,
   timer,
 }: GameOverPageProps) => {
-  const history = useHistory();
-
   const [hover, setHover] = useState(false);
+  const [gradeSaved, setGradeSaved] = useState(false);
 
   const userIsLogedIn = useSelector((state: RootStateOrAny) => state.auth.userId);
 
@@ -38,55 +38,62 @@ const GameOverPage = ({
   }
 
   return (
-    <section className='game-over' >
-      <h2 className='epilogue' >Τέλος καὶ τῷ Θεῷ Δόξα!</h2>
-      <h3 className='stadium' >Στάδιο: {stadiumCounter}ο</h3>
-      <h4 className='grade' >Τελική βαθμολογία: {totalPoints} </h4>
-      {timer && (
-        <>
-          <div onClick={() => {
-            alert(`Η βαθμολογία προκύπτει από το άθροισμα των μονάδων 
+    <>
+      <Prompt
+        when={!gradeSaved}
+        message='Αν θέλετε να αποθηκευθεί η βαθμολογία σας στη βάση δεδομένων, πατήστε εδώ άκυρο και μετά στη σελίδα το κουμπί "Αποθήκευση!" '
+      />
+      <section className='game-over' >
+        <h2 className='epilogue' >Τέλος καὶ τῷ Θεῷ Δόξα!</h2>
+        <h3 className='stadium' >Στάδιο: {stadiumCounter}ο</h3>
+        <h4 className='grade' >Τελική βαθμολογία: {totalPoints} </h4>
+        {timer && (
+          <>
+            <div onClick={() => {
+              alert(`Η βαθμολογία προκύπτει από το άθροισμα των μονάδων 
 (των σωστών απαντήσεων) 
 με τον χρόνο (που απέμεινε) επί 10.`)
-          }}>
+            }}>
 
-            {/* <MaterialIcons
+              {/* <MaterialIcons
                   name="info-outline"
                   size={Math.ceil(width * 0.08)}
                   color={Colours.maroon}
                 /> */}
+            </div>
+            <h5>
+              Χρόνος που απέμεινε: {minutesLeft}': {secondsLeft}''
+            </h5>
+          </>
+        )}
+        {userIsLogedIn ? (
+          <div className='instruction-text' >
+            <h5>
+              Αποθήκευση αποτελέσματος
+            </h5>
+            <h5>
+              και επιστροφή στην αρχική οθόνη
+            </h5>
           </div>
-          <h5>
-            Χρόνος που απέμεινε: {minutesLeft}': {secondsLeft}''
-          </h5>
-        </>
-      )}
-      {userIsLogedIn ? (
-        <div className='instruction-text' >
-          <h5>
-            Αποθήκευση αποτελέσματος
-          </h5>
-          <h5>
-            και επιστροφή στην αρχική οθόνη
-          </h5>
-        </div>
-      ) : (
-        <span>Eπιστροφή στην αρχική οθόνη:</span>
-      )}
-      {isUpLoading ? (
-        // <ActivityIndicator size="small" color={Colours.moccasin_light} />
-        <Loader />
-      ) : (
-        <Button
-          disabled={false}
-          title='Επιστροφή'
-          onClick={saveStadiumResult}
-          // onClick={() => history.replace(navNames.home)}
-          onMouseEnter={() => handleHover()}
-          onMouseLeave={() => handleHover()}
-        />
-      )}
-    </section>
+        ) : (
+          <span>Eπιστροφή στην αρχική οθόνη:</span>
+        )}
+        {isUpLoading ? (
+          <Loader />
+        ) : (
+          <Button
+            disabled={false}
+            title='Αποθήκευση'
+            onClick={() => {
+              setGradeSaved(true);
+              saveStadiumResult()
+            }}
+            onMouseEnter={() => handleHover()}
+            onMouseLeave={() => handleHover()}
+          />
+        )}
+      </section>
+    </>
   );
 };
 
